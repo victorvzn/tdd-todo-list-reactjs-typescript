97d6de834f282d03e9e4dd6b4c5e0e5c93557ff6
Victor Villazón <victorzn23@gmail.com>
2023-01-16 11:33:46 -0500

    test: it add new item to the list (passed)

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,8 +1,39 @@
+import React, { useState } from "react"
+
+import { v4 as uuid } from 'uuid'
+
 export interface TodoItem {
-  id: string,
+  id: string
   content: string
 }
 
 export const Todo = ({ items }: { items: TodoItem[] }) => {
-  return <div>{items.map(item => <span>{item.content}</span>)}</div>
+  const [todo, setTodo] = useState<string>('')
+  const [todos, setTodos] = useState<TodoItem[]>(items)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodo(e.target.value)
+  }
+
+  const handleKeyDown =  (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      const id = uuid()
+      setTodos([...todos, { id, content: todo }])
+    }
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        data-testid='todo-input'
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+
+      {todos.map(item => (
+        <span key={item.id}>{item.content}</span>
+      ))}
+    </div>
+  )
 }
