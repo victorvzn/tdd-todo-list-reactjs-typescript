1da979d7d1b0f103cd81b9bddd355fc417f55c2e
Victor Villazón <victorzn23@gmail.com>
2023-01-16 09:02:06 -0500

    test: it renders multiple items (failed)

--- src/components/Todo.test.tsx
+++ src/components/Todo.test.tsx
@@ -6,6 +6,10 @@ import { render, screen } from '@testing-library/react'
 // 3. Add new item to the list
 // 4. Mark an item as done
 
+const Todo = ({ items }: { items: string[] }) => {
+  return <div>{items[0]}</div>
+}
+
 describe('Todo list app', () => {
   it('renders an item', () => {
     const todos = ['buy some milk']
@@ -13,5 +17,22 @@ describe('Todo list app', () => {
     render(<Todo items={todos} />)
 
     expect(screen.getByText('buy some milk')).toBeInTheDocument()
+  })
+
+  it('renders another item', () => {
+    const todos = ['buy some apples']
+
+    render(<Todo items={todos} />)
+
+    expect(screen.getByText('buy some apples')).toBeInTheDocument()
+  })
+
+  it('renders multiple items', () => {
+    const todos = ['buy some milk', 'buy some apples']
+
+    render(<Todo items={todos} />)
+
+    expect(screen.getByText('buy some milk')).toBeInTheDocument()
+    expect(screen.getByText('buy some apples')).toBeInTheDocument()
   })
 })
